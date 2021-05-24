ded1ed07d24ba5ee1af15d105f97ac896902a6ff
Matt Bongiorno <bigiorno@gmail.com>
2021-05-24 14:12:36 -0400

    Navbar setup

---
+++ reduct/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [{
+        title: 'home',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Our Team',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'mobile home',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
